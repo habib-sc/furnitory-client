8921c181fe93be454821be7ce65ce1ac46248f9f
habib-sc <habib97.cs@gmail.com>
2022-05-01 01:39:42 +0600

    showing nav item based on auth user

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,13 @@
+import { signOut } from 'firebase/auth';
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
     const navigate = useNavigate();
+    const [user, loading, error] = useAuthState(auth);
 
     function MyLink({ children, to, ...props }: LinkProps) {
         let resolved = useResolvedPath(to);
@@ -37,11 +41,26 @@ const Header = () => {
                 <nav className={`md:ml-auto md:flex text-base justify-center md:static ${toggleMenu? 'top-16 bg-gray-100 w-full mt-2' : 'hidden'}`}>
                     <MyLink to='/' className="mr-5 hover:text-gray-900">Home</MyLink>
                     <MyLink to='/blogs' className="mr-5 hover:text-gray-900">Blogs</MyLink>
-                    <MyLink to='/inventory' className="mr-5 hover:text-gray-900">Manage Inventory</MyLink>
-                    <button onClick={ () => navigate('/login') } className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-400 rounded text-white mt-4 md:mt-0 mr-3">Login
+                    {user &&
+                      <>
+                        <MyLink to='/inventory' className="mr-5 hover:text-gray-900">Manage Items</MyLink>
+                        <MyLink to='/inventory' className="mr-5 hover:text-gray-900">Add Items</MyLink>
+                        <MyLink to='/inventory' className="mr-5 hover:text-gray-900">My Items</MyLink>
+                      </>
+                    }
+
+                    {user ?
+                    <button onClick={ () => signOut(auth) } className="inline-flex items-center bg-orange-500 border-0 py-1 px-3 focus:outline-none hover:bg-orange-400 rounded text-white mt-4 md:mt-0 mr-3">Logout
                     </button>
-                    <button onClick={ () => navigate('/register') } className="inline-flex items-center bg-orange-500 border-0 py-1 px-3 focus:outline-none hover:bg-orange-400 rounded text-white mt-4 md:mt-0">Register
-                    </button>
+                    : 
+                    <>
+                      <button onClick={ () => navigate('/login') } className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-400 rounded text-white mt-4 md:mt-0 mr-3">Login
+                      </button>
+                      <button onClick={ () => navigate('/register') } className="inline-flex items-center bg-orange-500 border-0 py-1 px-3 focus:outline-none hover:bg-orange-400 rounded text-white mt-4 md:mt-0">Register
+                      </button>
+                    </>
+
+                    }
                 </nav>
             </div>
         </header>
