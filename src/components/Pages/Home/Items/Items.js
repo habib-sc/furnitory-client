fab262d287550eee2095eb23ef493befcc714067
habib-sc <habib97.cs@gmail.com>
2022-05-02 02:53:34 +0600

    showing 6 items in home

--- src/components/Pages/Home/Items/Items.js
+++ src/components/Pages/Home/Items/Items.js
@@ -4,13 +4,15 @@ import Item from './Item/Item';
 
 const Items = () => {
     const [items, setItems] = useItems();
+    const limitedItems = items.splice(0, 6);
+
     return (
         <section>
             <h1 className="sm:text-3xl md:text-5xl font-bold title-font mb-4 text-gray-900 text-center py-10">Latest Inventory Items</h1>
             <div className='container mx-auto'>
                 <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
                     {
-                        items.map(item => <Item key={item._id} item={item}></Item>)
+                        limitedItems.map(item => <Item key={item._id} item={item}></Item>)
                     }
                 </div>
             </div>
